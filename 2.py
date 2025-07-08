39d819cd27b461bcc7192e388160df6449e38559
Jacob Valdez <jacobfv123@gmail.com>
2025-07-08 08:15:08 -0700

    save

--- 2.py
+++ 2.py
@@ -1,13 +1,87 @@
-from typing import Generator
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Generator, Self
 import networkx as nx
-from openrl.envs import Env
+import numpy as np
 
-Program = str
-Graph = nx.Graph
-
-
-def code2graph(code: str) -> nx.Graph:
+def parse_code_graph(code: str) -> nx.Graph:
     ...
+
+def render_code_graph(code_graph: nx.Graph) -> str:
+    ...
+
+'''
+Program Graph:
+- control pointer node: node that points to the next node to execute
+- control nodes: nodes that control rules match up with
+- data nodes: other nodes
+'''
+
+# @dataclass
+# class BaseProgramNode:
+#     pass
+# class BaseStatementProgramNode(BaseProgramNode):
+#     pass
+# class ReturnStatementProgramNode(BaseStatementProgramNode):
+#     pass
+# class AssignmentStatementProgramNode(BaseStatementProgramNode):
+#     pass
+# class IfStatementProgramNode(BaseStatementProgramNode):
+#     pass
+# class PrintStatementProgramNode(BaseStatementProgramNode):
+#     pass
+# class ReadStatementProgramNode(BaseStatementProgramNode):
+#     pass
+# class DeclarationProgramNode(BaseStatementProgramNode):
+#     pass
+# class FunctionCallProgramNode(BaseStatementProgramNode):
+#     pass
+# class BaseDataNode(BaseProgramNode):
+#     pass
+# class AtomicDataNode(BaseDataNode):
+#     pass
+# class ListDataNode(BaseDataNode):
+#     pass
+# class DictDataNode(BaseDataNode):
+#     pass
+
+class ProgramNodeType(Enum, str):
+    RETURN_STATEMENT = 'return_statement'
+    ASSIGNMENT_STATEMENT = 'assignment_statement'
+    IF_STATEMENT = 'if_statement'
+    PRINT_STATEMENT = 'print_statement'
+    READ_STATEMENT = 'read_statement'
+    DECLARATION_STATEMENT = 'declaration_statement'
+    FUNCTION_CALL_STATEMENT = 'function_call_statement'
+    ATOMIC_DATA = 'atomic_data'
+    LIST_DATA = 'list_data'
+    DICT_DATA = 'dict_data'
+
+@dataclass
+class Program:
+    program_graph: nx.Graph
+    locals: dict[str, nx.Graph] = field(default_factory=dict)
+    
+    def read_stdin(self: Self) -> nx.Graph:
+        return self.locals['stdin']
+    
+    def write_stdout(self: Self, value: nx.Graph):
+        self.locals['stdout'] = value
+    
+    def write_stderr(self: Self, value: nx.Graph):
+        self.locals['stderr'] = value
+
+    def set_exit_code(self: Self, value: int):
+        self.locals['exit_code'] = value
+
+    def step(self: Self) -> Self:
+        ...
+
+    def run(self: Self, max_steps: int) -> Self:
+        for _ in range(max_steps):
+            self.step()
+        return self
+
 
 # an object is a graph. an atomic object is just a 1-node graph
 # a program is a graph with control nodes
@@ -21,8 +95,7 @@ def code2graph(code: str) -> nx.Graph:
 # intelligence = 1 / (E[error] x experience x priors)
 
 class SingleProgramInstanceEnv(Env):
-    program_class: Program
-    initial_state: Graph
+    program_instance: Program
 
 
 class SingleProgramClassEnv(Env):
@@ -50,30 +123,16 @@ class ProgramGeneratorEnv(Env):
 
 
 
-def eval(ai_factory):
-
-    training_stats = []
-
-    def program_generator() -> Generator[Program, None, None]:
-        ...
-    
-    train(agent=meta_learning_agent, env=program_generator
-
-
-
-
-
-# in the future we will also make a generalization of this to have various rings of fixed and stationary characteristics
-
-
 '''
+in the future we will also make a generalization of this to have various rings of fixed and stationary characteristics
+
 every environment ring includes:
 - hidden state
 - observable state
 - action
 - forward dynamics
 
-program instance:
+program instance: this is a regular environment
 - hidden state: general search latents, program graph, locals[, standard graph trajectory]
 - observable state: [standard graph trajectory, ]standard graph output
 - action: standard graph input
